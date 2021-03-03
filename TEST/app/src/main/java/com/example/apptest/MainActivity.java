d300279084cb05d3179d031d4127c33427aff64a
DarkKnightEngineering <rozsraventio2305@gmail.com>
2021-03-03 13:07:18 +0800

    First Commit

---
+++ TEST/app/src/main/java/com/example/apptest/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.apptest;
+
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Mainactivity2();
+            }
+        });
+    }
+
+    public void Mainactivity2()
+    {
+        Intent intent = new Intent(this, MainActivity2.class);
+        startActivity(intent);
+    }
+}
